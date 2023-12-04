df1227faf49b0670e5f26eb22a6a89312432c678
Krathish sk <krathishs.k123@gmail.com>
2023-12-04 12:16:49 +0530

    Lower, Greater Login

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import { useState } from "react";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function genreateRandomNumber(min, max, exclude) {
   const num = Math.floor(Math.random() * (max - min)) + min;
@@ -11,16 +12,54 @@ function genreateRandomNumber(min, max, exclude) {
   } else return num;
 }
 
+let minBoundry = 1;
+let maxBoundry = 100;
+
 export default function GameScreen({ userNumber }) {
-  const initialGuess = genreateRandomNumber(1, 100, userNumber);
+  const initialGuess = genreateRandomNumber(minBoundry, maxBoundry, userNumber);
 
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === "lower" && currentGuess <= userNumber) ||
+      (direction === "greater" && currentGuess >= userNumber)
+    ) {
+      Alert.alert("Don't Lie", "You know that this is wrong...", [
+        { text: "Sorry", style: "default" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      maxBoundry = currentGuess;
+    } else {
+      minBoundry = currentGuess + 1;
+    }
+    const newNumber = genreateRandomNumber(
+      minBoundry,
+      maxBoundry,
+      currentGuess
+    );
+    setCurrentGuess(newNumber);
+  }
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower</Text>
+        <View>
+          <PrimaryButton
+            onPress={nextGuessHandler.bind(this, "greater")}
+            text={"+"}
+          />
+          <PrimaryButton
+            onPress={nextGuessHandler.bind(this, "lower")}
+            text={"-"}
+          />
+        </View>
       </View>
     </View>
   );
